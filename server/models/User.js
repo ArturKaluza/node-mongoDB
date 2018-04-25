990d3ea14380863b09171aa18d436d70c716d83d
Artur <akaluza26@gmail.com>
2018-04-25 22:36:04 +0200

    Add GET /todos route and test

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,12 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User', {
-    email: {
-        required: true,
-        trim: true,
-        minlength: 1,
-        type: String
-    }
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-// example of creating and saving new User
-// const John = new User({
-//     email: 'john23@test.com'
-// });
-
-// John.save().then((doc) => {
-//     console.log('Save success');
-//     console.log(doc);
-// }, (e) => {
-//     console.log('Unable to save', e);
-// });
-
-module.exports = {User};
+module.exports = {User}
