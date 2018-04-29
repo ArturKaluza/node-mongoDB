fa771fe4a24857f2140f1d366ea91602bbe730c0
Artur <akaluza26@gmail.com>
2018-04-29 11:56:26 +0200

    Added PATCH /todos/:id test and ENV variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const env = process.env.NODE_ENV || 'development';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://artur:123test123@ds159489.mlab.com:59489/todos');
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect('mongodb://artur:123test123@ds159489.mlab.com:59489/todos');
+} else {
+    mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 
 module.exports = {mongoose};
